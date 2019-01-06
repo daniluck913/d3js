123e823013a9ff55efc6f4fcd0cc34d06cbdf90c
daniluck913 <daniluck913@gmail.com>
2019-01-06 12:54:52 -0500

    Practicamos como dibujar datos con D3

--- 03 - Conceptos Básicos/05 - Dibujando con datos/app_inicio.js
+++ 03 - Conceptos Básicos/05 - Dibujando con datos/app_inicio.js
@@ -1,8 +1,13 @@
 var datos = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
 
 function graficar() {
-    d3.select('.miGrafica')
-        .selectAll('div')
-        .data(datos)
-        .enter().append('div')
+  d3.select(".miGrafica")
+    .selectAll("div")
+    .data(datos)
+    .enter()
+    .append("div")
+    .attr("class", "barra")
+    .style("height", function(d) {
+      return d + "px";
+    });
 }
