c3a0b263ba341e86246e78abc3dd4701b3251b0f
daniluck913 <daniluck913@gmail.com>
2019-01-06 12:15:13 -0500

    Pacticamos como encadenar metodos para optimizar el codigo y ahorrar tiempo

--- 03 - Conceptos Básicos/03 - Encadenar métodos/app_inicio.js
+++ 03 - Conceptos Básicos/03 - Encadenar métodos/app_inicio.js
@@ -1,3 +1,8 @@
-function seleccionarBloque(){
-
+function seleccionarBloque() {
+  d3.select(".bloque")
+    .selectAll("div")
+    .style("background-color", "blue")
+    .style("color", "white")
+    .style("padding", "5px")
+    .style("margin", "5px");
 }
