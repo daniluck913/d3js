5c0da7bbee39024bd9a3b94bed5dc8a7bb8486e0
daniluck913 <daniluck913@gmail.com>
2019-01-06 11:47:37 -0500

    Se guarda ejercicio de selectores, select y selectAll

---
+++ 03 - Conceptos Básicos/02 - Selectores/app_inicio.js
@@ -0,0 +1,9 @@
+function seleccionar() {
+  d3.selectAll("div").style("background-color", "green");
+}
+
+function seleccionarBloque() {
+  d3.select(".bloque")
+    .selectAll("div")
+    .style("background-color", "red");
+}
