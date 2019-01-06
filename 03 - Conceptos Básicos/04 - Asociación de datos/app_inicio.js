d51e07f41e96dd3140033ee8fc8f214f0c2045ec
daniluck913 <daniluck913@gmail.com>
2019-01-06 12:40:04 -0500

    Parcticamos la asociacón de datos

--- 03 - Conceptos Básicos/04 - Asociación de datos/app_inicio.js
+++ 03 - Conceptos Básicos/04 - Asociación de datos/app_inicio.js
@@ -1,3 +1,21 @@
-function graficar() {
+var datos = [1, 2, 3, 5, 8, 13, 21, 33, 55, 89, 114];
 
+function graficar() {
+  d3.select(".miGrafica")
+    .selectAll("div")
+    .data(datos)
+    .enter()
+    .append("div")
+    .text(function(d) {
+      if (d == 1) {
+        return "Ricardito ha tenido " + d + " Balón";
+      } else {
+        return "Ricardito ha tenido " + d + " Balones";
+      }
+    })
+    .style("background-color", "green")
+    .style("color", "white")
+    .style("padding", "5px")
+    .style("margin", "5px")
+    .style("text-align", "right");
 }
