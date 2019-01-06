1e684966571d0aa6077b34091367b232d5071149
daniluck913 <daniluck913@gmail.com>
2019-01-06 15:10:02 -0500

    Gráfica de barras con datos externos

--- 03 - Conceptos Básicos/08 - Gráficas de barras con SVG/app_inicio.js
+++ 03 - Conceptos Básicos/08 - Gráficas de barras con SVG/app_inicio.js
@@ -39,7 +39,7 @@ function graficar() {
     .append("rect")
     .attr("x", 0)
     .attr("y", 0)
-    .attr("width", 20)
+    .attr("width", 15)
     .attr("height", 100)
 
     .attr("x", function(d, i) {
--- 03 - Conceptos Básicos/09 - Cargando datos externos/app_inicio.js
+++ 03 - Conceptos Básicos/09 - Cargando datos externos/app_inicio.js
@@ -1,48 +1,63 @@
 var datos = [];
 
-function cargarDatos() {
+// function cargarDatos() {
+//   d3.json("datos.json", function(err, data) {
+//     datos = data;
+//     console.log(datos);
+//     graficar();
+//   });
+// }
 
+function cargarDatos() {
+  d3.csv("datos.csv", function(err, data) {
+    datos = data;
+    console.log(datos);
+    graficar();
+  });
 }
 
 function graficar() {
-    var w = 500;
-    var h = 300;
+  var w = 500;
+  var h = 300;
 
-    var svg = d3.select('body')
-        .append('svg')
-        .attr("width", w)
-        .attr("height", h);
+  var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-    svg.selectAll("rect")
-        .data(datos)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 20)
-        .attr("height", 100)
+  svg
+    .selectAll("rect")
+    .data(datos)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 20)
+    .attr("height", 100)
 
     .attr("x", function(d, i) {
-            return i * 21 + 30
-        })
-        .attr("height", function(d) {
-            return d.dato;
-        })
-        .attr("y", function(d) {
-            return h - d.dato;
-        })
+      return i * 21 + 30;
+    })
+    .attr("height", function(d) {
+      return d.dato;
+    })
+    .attr("y", function(d) {
+      return h - d.dato;
+    });
 
-    svg.selectAll("text")
-        .data(datos)
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d.dato;
-        })
-        .attr("x", function(d, i) {
-            return i * 21 + 40;
-        })
-        .attr("y", function(d) {
-            return h - d.dato - 3;
-        })
+  svg
+    .selectAll("text")
+    .data(datos)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d.dato;
+    })
+    .attr("x", function(d, i) {
+      return i * 21 + 40;
+    })
+    .attr("y", function(d) {
+      return h - d.dato - 3;
+    });
 }
