6669bd91eb15de6ffa6f7c64ebf9ebbbca39ac39
daniluck913 <daniluck913@gmail.com>
2019-01-06 20:18:55 -0500

    Practica con eventos

--- 06 - Animación e interacción/01 - Eventos/app_inicio.js
+++ 06 - Animación e interacción/01 - Eventos/app_inicio.js
@@ -1,59 +1,80 @@
-var datos = [144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+var datos = [
+  144,
+  89,
+  55,
+  34,
+  21,
+  13,
+  8,
+  5,
+  3,
+  2,
+  1,
+  2,
+  3,
+  5,
+  8,
+  13,
+  21,
+  34,
+  55,
+  89,
+  144
+];
 
 function graficar() {
-    var w = 500;
-    var h = 300;
+  var w = 500;
+  var h = 300;
 
-    var svg = d3.select('body')
-        .append('svg')
-        .attr("width", w)
-        .attr("height", h);
+  var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
 
-    svg.selectAll("rect")
-        .data(datos)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", 20)
-        .attr("height", 100)
+  svg
+    .selectAll("rect")
+    .data(datos)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 20)
+    .attr("height", 100)
 
     .attr("x", function(d, i) {
-        return i * 21 + 30;
+      return i * 21 + 30;
     })
 
     .attr("height", function(d) {
-        return d + 50;
+      return d + 50;
     })
 
     .attr("y", function(d) {
-            return h - d - 50;
-        })
+      return h - d - 50;
+    })
     .attr("fill", "SteelBlue")
 
-    .on("mouseover", function(){
-        d3.select(this)
-        .attr("fill", "tomato")
+    .on("mouseover", function() {
+      d3.select(this).attr("fill", "tomato");
     })
 
-    .on("mouseout", function(){
-        d3.select(this)
-        .attr("fill", "SteelBlue")
+    .on("mouseout", function() {
+      d3.select(this).attr("fill", "green");
+    });
+
+  svg
+    .selectAll("text")
+    .data(datos)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d;
     })
-
-
-
-    svg.selectAll("text")
-        .data(datos)
-        .enter()
-        .append("text")
-        .text(function(d) {
-            return d;
-        })
-        .attr("x", function(d, i) {
-            return i * 21 + 40;
-        })
-        .attr("y", function(d) {
-            return h - d - 53;
-        })
+    .attr("x", function(d, i) {
+      return i * 21 + 40;
+    })
+    .attr("y", function(d) {
+      return h - d - 53;
+    });
 }
